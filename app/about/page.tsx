9ff587ef90ce0e295879d3b3e36dfdd972bd4157
Krystian Kowalski <krycha1515@gmail.com>
2023-04-16 22:38:48 +0200

    Add metadata to About page, finish About component

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,15 @@
 import Breadcrumbs from "@/components/Breadcrumbs/Breadcrumbs";
 import About from "@/components/About/About";
 
+export const metadata = {
+	title: "Krycha Store - About",
+	description:
+		"Discover the latest trends in Krycha store, including women, men and kids clothing. Shop the best outfits for this season at our online store",
+	icons: {
+		icon: "/cloth-icon.png",
+	},
+};
+
 const AboutPage = () => {
 	return (
 		<>
--- components/About/About.tsx
+++ components/About/About.tsx
@@ -1,8 +1,37 @@
-import styles from "./About.module.scss"
+import Link from "next/link";
+import styles from "./About.module.scss";
+import Image from "next/image";
 
 const About = () => {
-  return (
-    <div>About</div>
-  )
-}
-export default About
+	return (
+		<>
+			<section className={styles.about}>
+				<div className={styles.about__overlay}></div>
+				<div className={styles.about__content}>
+					<p>NEW COLLECTION</p>
+					<p>Be different in your own way!</p>
+					<p>Find your unique style.</p>
+					<Link href="/products">Shop Collection</Link>
+				</div>
+			</section>
+			<section className={styles.story}>
+				<Image
+					src="/collection-01.jpg"
+					alt="Woman model"
+					width={590}
+					height={690}
+				/>
+				<article className={styles.story__content}>
+					<h2>Story</h2>
+					<p>
+						Krycha Store is a great spot to find classic, timeless pieces for
+						men, women and kids. From comfy joggers and denim jeans to stylish
+						blouses and dress shirts, you'll find everything you need for a
+						polished wardrobe.
+					</p>
+				</article>
+			</section>
+		</>
+	);
+};
+export default About;
