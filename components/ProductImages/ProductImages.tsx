5037f98db5be6bad5ff3c48d46e638e3e6283431
Krystian Kowalski <krycha1515@gmail.com>
2023-04-18 14:49:13 +0200

    Add feature to change active image and style ProductImages component

--- components/ProductImages/ProductImages.tsx
+++ components/ProductImages/ProductImages.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import { useState } from "react";
+import Image from "next/image";
 import styles from "./ProductImages.module.scss";
 
 type ProductImagesProps = {
@@ -5,6 +9,33 @@ type ProductImagesProps = {
 };
 
 const ProductImages = ({ images }: ProductImagesProps) => {
-	return <div>ProductImages</div>;
+	const [mainImg, setMainImg] = useState(images[2]);
+
+	return (
+		<section className={styles.images}>
+			<Image
+				src={mainImg?.url + ".jpg"}
+				alt="product main image"
+				width={600}
+				height={600}
+				className={styles.images__mainImage}
+			/>
+			<div className={styles.images__gallery}>
+				{images?.map((image, index) => (
+					<Image
+						key={image.id}
+						src={image?.url + ".jpg"}
+						alt="product alt image"
+						width={600}
+						height={600}
+						onClick={() => setMainImg(images[index])}
+						className={`${styles.images__altImage} ${
+							mainImg.url === image.url && styles.active
+						}`}
+					/>
+				))}
+			</div>
+		</section>
+	);
 };
 export default ProductImages;
